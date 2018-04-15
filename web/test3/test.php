8fb8d6acb0c243967162873042eab3282c5c1e8d
talktodhi <talktodhi@gmail.com>
2018-04-15 13:01:16 +0530

    Changes: Test file

--- web/test3/test.php
+++ web/test3/test.php
@@ -10,13 +10,12 @@ function Send_Mail($to,$subject,$body)
 	$mail->IsSMTP(true); // SMTP
 	$mail->SMTPAuth   = true;  // SMTP authentication
 	$mail->Mailer = "smtp";
-	$mail->Host= "tls://email-smtp.us-east.amazonaws.com"; // Amazon SES
+	$mail->Host= "tls://email-smtp.us-west-2.amazonaws.com"; // Amazon SES
 	$mail->Port = 465;  // SMTP Port
-	$mail->Username = "Your_SMTP_Username
-	";  // SMTP  Username
-	$mail->Password = "SMTP_Password";  // SMTP Password
-	$mail->SetFrom($from, 'From Name');
-	$mail->AddReplyTo($from,'yourdomain.com or verified email address');
+	$mail->Username = "AKIAIKQEPFJEW7GCTCYQ";  // SMTP  Username
+	$mail->Password = "Aiqkpzd3U8sKOcXK4dn2HfqXn0TZVbzgEQeQS7Yt/NDp";  // SMTP Password
+	$mail->SetFrom($from, 'Name');
+	//$mail->AddReplyTo($from,'yourdomain.com or verified email address');
 	$mail->Subject = $subject;
 	$mail->MsgHTML($body);
 	$address = $to;
