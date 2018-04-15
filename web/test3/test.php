9ab935ec2e7af7f64b11422ea9978e5dd99eaea3
talktodhi <talktodhi@gmail.com>
2018-04-15 13:38:30 +0530

    Changes: test file

--- web/test3/test.php
+++ web/test3/test.php
@@ -13,12 +13,12 @@ error_reporting(E_ALL);
 	$mail->IsSMTP(true); // SMTP
 	$mail->SMTPAuth   = true;  // SMTP authentication
 	$mail->Mailer = "smtp";
-	$mail->Host= "tls://email-smtp.us-west-2.amazonaws.com"; // Amazon SES
+	$mail->Host= "email-smtp.us-west-2.amazonaws.com"; // Amazon SES
 	$mail->Port = 465;  // SMTP Port
 	$mail->Username = "AKIAIKQEPFJEW7GCTCYQ";  // SMTP  Username
 	$mail->Password = "Aiqkpzd3U8sKOcXK4dn2HfqXn0TZVbzgEQeQS7Yt/NDp";  // SMTP Password
 	$mail->SetFrom($from, 'Name');
-	//$mail->AddReplyTo($from,'yourdomain.com or verified email address');
+	$mail->AddReplyTo($from,'dhiraj.bastwade@gmail.com');
 	$mail->Subject = $subject;
 	$mail->MsgHTML($body);
 	$address = $to;
